65b8fce90b46b2e66daea7428dacce409a73585e
wskingdom <71829150+wskingdom@users.noreply.github.com>
2023-06-18 22:35:20 +0800

    Delete lsc-cnn directory

--- lsc-cnn/utils/mle_function/script.sh
+++
@@ -1,2 +0,0 @@
-python setup.py build
-mv build/lib.linux-x86_64-3.6/error_function.cpython-36m-x86_64-linux-gnu.so ../../error_function.so
--- lsc-cnn/work.py
+++
@@ -1,7 +0,0 @@
-import os
-img_root = r'/home/ubuntu/dataset/Train'
-img_list = sorted([name for name in os.listdir(img_root)])
-for name in img_list:
-    img_path = os.path.join(img_root,name)
-    new_path = os.path.join(img_root,'qnrf_train_'+name.split('_')[1].replace('jpg','jpeg'))
-    os.rename(img_path,new_path)
